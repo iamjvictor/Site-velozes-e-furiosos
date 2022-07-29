e1db1d2322e6a16b98310366e8b48c628a8b98d1
João Victor Asevedo <joao-victor_07@outlook.com>
2022-07-29 18:02:41 -0300

    Criado a pagina de registros e feita a conexão com o banco de dados

--- html-css/routes/routes.js
+++ html-css/routes/routes.js
@@ -5,6 +5,8 @@ routes.get("/", controller.getPage );
 routes.get("/fotosvf.ejs", controller.getPic);
 routes.get("/homenagem.ejs", controller.getHom);
 routes.get("/community.ejs", controller.getCom);
+routes.get("/register.ejs", controller.getRegister);
+
 
 
 module.exports = routes ;
